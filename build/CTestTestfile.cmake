2f692d5dc4a16052c94c92df4c60b3bfcefa8dfb
csanrod <c.sanchezro.2018@alumnos.urjc.es>
2022-07-25 10:39:27 +0200

    Adding .gitignore to the repository

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/csanrod/TFG/2022-tfg-cristian-sanchez/src
-# Build directory: /home/csanrod/TFG/2022-tfg-cristian-sanchez/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("teleop")
--- build/teleop/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/csanrod/TFG/2022-tfg-cristian-sanchez/src/teleop
-# Build directory: /home/csanrod/TFG/2022-tfg-cristian-sanchez/build/teleop
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
