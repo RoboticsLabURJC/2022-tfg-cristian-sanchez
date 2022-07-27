045637a6420a1a4c2254683348cc576d7e25bfdc
csanrod <c.sanchezro.2018@alumnos.urjc.es>
2022-07-27 12:14:28 +0200

    added turns, land, fw, bw buttons, land not working

--- src/teleop/scripts/radio_control.py
+++ src/teleop/scripts/radio_control.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import cv2
 import rospy
+import tf
 from geometry_msgs.msg import Twist, PoseStamped
 
 MAX = 100
@@ -9,7 +10,10 @@ PI = 3.1416
 LINEAR_FACTOR = 2 / 100
 ANGULAR_FACTOR = (2 * PI) / 100
 WINDOWNAME = 'Radio Control'
- 
+
+yaw = 0
+
+# -- SLIDERS -- # 
 def linear_x_change(val):
     vel.linear.x = (val * LINEAR_FACTOR) - (MID * LINEAR_FACTOR)
     vel_pub.publish(vel)
@@ -26,23 +30,85 @@ def angular_z_change(val):
     vel.angular.z = (val * ANGULAR_FACTOR) - (MID * ANGULAR_FACTOR)
     vel_pub.publish(vel)
 
-# def launch_switch(button):
-#     print(button)
-
-# def land_switch(button):
-#     print(button)
-
-# def turn_cw_switch(button):
-#     print(button)
-
-# def turn_acw_switch(button):
-#     print(button)
-
+# -- BUTTONS -- #
 def launch_button(*args):
     set_sliders_to_default()
     pos.pose.position.z = 2
     pos_pub.publish(pos)
 
+def land_button(*args):
+    # Not working...
+    set_sliders_to_default()
+    pos.pose.position.z = 0
+    pos_pub.publish(pos)
+
+def turnCW_button(*args):
+    global yaw
+    set_sliders_to_default()
+
+    yaw += PI/2
+
+    if yaw == 2*PI:
+        yaw = 0
+
+    quaternion = tf.transformations.quaternion_from_euler(0, 0, yaw)
+
+    pos.pose.orientation.x = quaternion[0]
+    pos.pose.orientation.y = quaternion[1]
+    pos.pose.orientation.z = quaternion[2]
+    pos.pose.orientation.w = quaternion[3]
+
+    pos_pub.publish(pos)
+
+def turnACW_button(*args):
+    global yaw
+    set_sliders_to_default()
+
+    yaw -= PI/2
+
+    if yaw == -2*PI:
+        yaw = 0
+
+    quaternion = tf.transformations.quaternion_from_euler(0, 0, yaw)
+
+    pos.pose.orientation.x = quaternion[0]
+    pos.pose.orientation.y = quaternion[1]
+    pos.pose.orientation.z = quaternion[2]
+    pos.pose.orientation.w = quaternion[3]
+
+    pos_pub.publish(pos)
+
+def FW_button(*args):
+    global yaw
+    set_sliders_to_default()
+
+    if yaw == 0:
+        pos.pose.position.x += 2
+    elif yaw == PI/2 or yaw == -(3*PI)/2:
+        pos.pose.position.y += 2
+    elif yaw == PI or yaw == -PI:
+        pos.pose.position.x -= 2
+    elif yaw == (3*PI)/2 or yaw == -PI/2:
+        pos.pose.position.y -= 2
+
+    pos_pub.publish(pos)
+
+def BW_button(*args):
+    global yaw
+    set_sliders_to_default()
+
+    if yaw == 0:
+        pos.pose.position.x -= 2
+    elif yaw == PI/2 or yaw == -(3*PI)/2:
+        pos.pose.position.y -= 2
+    elif yaw == PI or yaw == -PI:
+        pos.pose.position.x += 2
+    elif yaw == (3*PI)/2 or yaw == -PI/2:
+        pos.pose.position.y += 2
+
+    pos_pub.publish(pos)
+
+# -- OTHER -- #
 def set_sliders_to_default():
     sliderNames = ('Vx', 'Vy', 'Vz', 'Wz')
     
@@ -53,6 +119,7 @@ if __name__ == '__main__':
     try:
         vel = Twist()
         pos = PoseStamped()
+        
 
         vel_pub = rospy.Publisher('rc/vel', Twist, queue_size=10)
         pos_pub = rospy.Publisher('rc/pos', PoseStamped, queue_size=10)
@@ -69,13 +136,11 @@ if __name__ == '__main__':
 
         # Switches
         cv2.createButton('Launch', launch_button, None, cv2.QT_PUSH_BUTTON, 1)
-
-
-        # cv2.createTrackbar('Launc', WINDOWNAME, 0, 1, launch_switch)
-        # cv2.createTrackbar('Land', WINDOWNAME, 0, 1, land_switch)
-        # cv2.createTrackbar('Turn 90º', WINDOWNAME, 0, 1, turn_cw_switch)
-        # cv2.createTrackbar('Turn -90º', WINDOWNAME, 0, 1, turn_acw_switch)
-
+        cv2.createButton('Land', land_button, None, cv2.QT_PUSH_BUTTON, 1)
+        cv2.createButton('90 deg', turnCW_button, None, cv2.QT_PUSH_BUTTON, 1)
+        cv2.createButton('-90 deg', turnACW_button, None, cv2.QT_PUSH_BUTTON, 1)
+        cv2.createButton('Fordward 2m', FW_button, None, cv2.QT_PUSH_BUTTON, 1)
+        cv2.createButton('Backward 2m', BW_button, None, cv2.QT_PUSH_BUTTON, 1)
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
