b7f11af142dc9c2ef4bcba1f2a04f544a1bac4c5
csanrod <c.sanchezro.2018@alumnos.urjc.es>
2022-09-22 11:09:06 +0200

    Capsuled functionality for markers

--- src/heatmap_util/scripts/heatmap.py
+++ src/heatmap_util/scripts/heatmap.py
@@ -7,44 +7,48 @@ NODENAME = 'heatmap_node'
 MARKER_TOPIC = 'heatmap_markers'
 FRAME_ID = 'map'
 
-if __name__ == '__main__':
-    rospy.init_node(NODENAME, anonymous=True)
-    marker_pub = rospy.Publisher(MARKER_TOPIC, Marker, queue_size=10)
-    rate = rospy.Rate(10)
-
+def init_marker():
     marker = Marker()
 
     marker.header.frame_id = FRAME_ID
     marker.header.stamp = rospy.Time.now()
-
     marker.id = 0
     marker.ns = 'Cube'
     marker.action = marker.ADD
     marker.type = marker.CUBE
-    marker.lifetime = rospy.Duration(0.0)    
-
+    marker.lifetime = rospy.Duration(0.0)
     # marker position
     marker.pose.position.x = 0.0
     marker.pose.position.y = 0.0
     marker.pose.position.z = 0.0
-
     # marker pose
     marker.pose.orientation.x = 0.0
     marker.pose.orientation.y = 0.0
     marker.pose.orientation.z = 0.0
     marker.pose.orientation.w = 1.0
-
     # marker scale
     marker.scale.x = 2.0
     marker.scale.y = 2.0
-    marker.scale.z = 0.01
-
-    # marker colors
+    marker.scale.z = 0.001
     # a (alpha) must be non-zero
     marker.color.a = 1.0
-    marker.color.r = 1.0
-    marker.color.g = 0.0
-    marker.color.b = 0.0
+
+    return marker
+
+def set_marker_rgb(marker, R, G, B):
+    # marker colors   
+    marker.color.r = R
+    marker.color.g = G
+    marker.color.b = B
+
+
+if __name__ == '__main__':
+    rospy.init_node(NODENAME, anonymous=True)
+    marker_pub = rospy.Publisher(MARKER_TOPIC, Marker, queue_size=10)
+    rate = rospy.Rate(10)
+
+    marker = init_marker()
+    set_marker_rgb(marker, 0.0, 1.0, 0.0)
 
     while not rospy.is_shutdown():
         # publish msg
