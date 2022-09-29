601d3018b5001df41e97983a035112fab4a29b28
csanrod <c.sanchezro.2018@alumnos.urjc.es>
2022-09-29 13:08:43 +0200

    optimized a little bit

--- src/heatmap_util/scripts/heatmap.py
+++ src/heatmap_util/scripts/heatmap.py
@@ -1,22 +1,34 @@
 #! /usr/bin/env python
+'''
+HEATMAP
 
+This app is made for debbuging purposes, it draws
+markers inside rviz, following px4 trayectory.
+
+Is made to work with center 2 center control script.
+'''
 import rospy
 from visualization_msgs.msg import Marker, MarkerArray
 from geometry_msgs.msg import PoseStamped
 import random
 
+# -- CTE -- #
 NODENAME = 'heatmap_node'
 MARKER_TOPIC = 'heatmap_markers'
 FRAME_ID = 'map'
 LOCAL_POSE_TOPIC = '/mavros/local_position/pose'
 
+# -- GLOBAL VARIABLES -- #
 unique_marker_id = 0
 current_pos = PoseStamped()
 markers = MarkerArray()
-markers_pose = []
 i = 0
+poses = []
 
 def init_marker():
+    '''
+    Init default rviz marker parameters
+    '''
     global unique_marker_id
 
     marker = Marker()
@@ -53,98 +65,82 @@ def init_marker():
     return marker
 
 def set_marker_rgb(marker):
+    '''
+    Set rviz marker color, in this case random
+    '''
     marker.color.r = random.random()
     marker.color.g = random.random()
     marker.color.b = random.random()
 
 
 def set_marker_pose(marker, x, y, z):
-    # Same pose fix ??
+    '''
+    Set rviz marker position
+    '''
     marker.pose.position.x = x
     marker.pose.position.y = y
     marker.pose.position.z = z
-
-def del_marker(marker, publisher):
-    # Not working...
-    marker.action = marker.DELETEALL
-    publisher.publish(marker)
 
 def scan_cell():
     '''
     Scans current cell, and returns markers list:
 
         - marker, store rviz marker to add it to markers list.
-        - marker_pose, store the pose of the rviz marker previously defined.
-          Then is added to the markers_pose list.
+        - marker_pose, store the x, y coords of the rviz marker previously defined.
+          Then is added to the poses list.
     '''
-    global markers, markers_pose, unique_marker_id, current_pos, i
+    global markers, unique_marker_id, current_pos, i, poses
 
     if unique_marker_id == 0:
         # Origin marker placement
-        marker_pose = PoseStamped()
-
-        marker_pose.pose.position.x = 0.0
-        marker_pose.pose.position.y = 0.0
-        marker_pose.pose.position.z = 0.0
-
         marker = init_marker() 
         set_marker_rgb(marker)
-        set_marker_pose(marker, 0.0, 0.0, 0.0)
+        set_marker_pose(marker, 0, 0, 0)
 
         markers.markers.append(marker)
-        markers_pose.append(marker_pose)
+        poses.append((0, 0))
     else:
+        # Last x, y visited
+        x = poses[i][0]
+        y = poses[i][1]
+
         # Inside last cell marked conditions
-        inside_cell_x = (markers_pose[i].pose.position.x - 1) <= current_pos.pose.position.x <= (markers_pose[i].pose.position.x + 1)
-        inside_cell_y = (markers_pose[i].pose.position.y - 1) <= current_pos.pose.position.y <= (markers_pose[i].pose.position.y + 1)
+        inside_cell_x = (x - 1) <= current_pos.pose.position.x <= (x + 1)
+        inside_cell_y = (y - 1) <= current_pos.pose.position.y <= (y + 1)
 
         if not inside_cell_x or not inside_cell_y:           
-            marker_pose = PoseStamped()
-
-            # z CTE
-            marker_pose.pose.position.z = 0.0
-
             if not inside_cell_x:
-                # y CTE if it's out of x range
-                marker_pose.pose.position.y = markers_pose[i].pose.position.y
-
+                # y CTE if x is out of range
+                new_y = y
                 # Condition to evalue if drone is inside right cell or not (left cell).
-                inside_right_cell = current_pos.pose.position.x > (markers_pose[i].pose.position.x + 1)                
+                inside_right_cell = current_pos.pose.position.x > (x + 1)                
                 if inside_right_cell:
-                    marker_pose.pose.position.x = markers_pose[i].pose.position.x + 2
+                    new_x = x + 2
                 else:
-                    marker_pose.pose.position.x = markers_pose[i].pose.position.x - 2
+                    new_x = x - 2
 
             elif not inside_cell_y:
-                # x CTE if it's out of y range
-                marker_pose.pose.position.x = markers_pose[i].pose.position.x
-
+                # x CTE if y is out of range
+                new_x = x
                 # Condition to evalue if drone is inside upper cell or not (bottom cell).
-                inside_upper_cell = current_pos.pose.position.y > (markers_pose[i].pose.position.y + 1)
+                inside_upper_cell = current_pos.pose.position.y > (y + 1)
                 if inside_upper_cell:
-                    marker_pose.pose.position.y = markers_pose[i].pose.position.y + 2
+                    new_y = y + 2
                 else:
-                    marker_pose.pose.position.y = markers_pose[i].pose.position.y - 2
+                    new_y = y - 2
 
-            # Not efficient!!
-            if marker_pose not in markers_pose:
-                i = unique_marker_id
-
+            if (new_x, new_y) not in poses:
                 marker = init_marker() 
                 set_marker_rgb(marker)
-                set_marker_pose(marker, 
-                                marker_pose.pose.position.x, 
-                                marker_pose.pose.position.y, 
-                                marker_pose.pose.position.z)
+                set_marker_pose(marker, new_x, new_y, 0)
 
                 markers.markers.append(marker)
-                markers_pose.append(marker_pose)
-            else:
-                i -= 1
+                poses.append((new_x, new_y))
+
+            i = poses.index((new_x, new_y))
 
     return markers
 
-# Callback
 def current_pos_cb(pose):
     '''
     Local pose callback.
@@ -152,14 +148,15 @@ def current_pos_cb(pose):
     global current_pos
     current_pos = pose
 
+# -- MAIN -- #
 if __name__ == '__main__':
     rospy.init_node(NODENAME, anonymous=True)
 
+    # ROS pub/sub
     markers_pub = rospy.Publisher(MARKER_TOPIC, MarkerArray, queue_size=10)
     current_pos_sub = rospy.Subscriber(LOCAL_POSE_TOPIC, PoseStamped, callback = current_pos_cb)
 
     rate = rospy.Rate(10)
-
     while not rospy.is_shutdown():
         markers_pub.publish(scan_cell())
         rate = rospy.Rate(10)
