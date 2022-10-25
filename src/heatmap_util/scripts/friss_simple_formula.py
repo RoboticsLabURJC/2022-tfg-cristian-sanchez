d9659a36bf1fdcb751cf6fa97303da17735c32ce
csanrod <c.sanchezro.2018@alumnos.urjc.es>
2022-10-25 14:17:03 +0200

    added launcher with radio tower model, completed friss simulation, need to scale the model

--- src/heatmap_util/scripts/friss_simple_formula.py
+++ src/heatmap_util/scripts/friss_simple_formula.py
@@ -17,3 +17,20 @@ P_r = P_t * (G_t * G_r * (lmd)**2)/(((4 * np.pi)**2) * (d**n) * L)
 # -- PROPAGATION PATH LOSS -- #
 # Empty space
 P_l = -10 * np.log10((lmd**2)/((4 * np.pi * d)**2))
+
+grid = np.zeros((9, 9))
+
+x_origin, y_origin = (5, 5)
+x_l, y_l = grid.shape
+for x in range(x_l):
+    for y in range(y_l):
+        d = ((x - x_origin)**2 + (y - y_origin)**2)**(1/2)
+        if d == 0:
+            # grid[x, y] = 0.0
+            grid[x, y] = 1.0
+        else:
+            # grid[x, y] = -10 * np.log10((lmd**2)/((4 * np.pi * d)**2))        
+            grid[x, y] = P_t * (G_t * G_r * (lmd)**2)/(((4 * np.pi)**2) * (d**n) * L)
+
+grid = np.round(grid, 2)
+print(grid)
