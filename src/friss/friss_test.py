57503b8489328f8a0abe66a9c730281f2ca84ad8
csanrod <c.sanchezro.2018@alumnos.urjc.es>
2023-08-17 11:54:47 +0200

    intersections working but it needs a method to select the correct one

--- src/friss/friss_test.py
+++ src/friss/friss_test.py
@@ -13,6 +13,7 @@ class Point:
         self.x = x
         self.y = y
 
+
 class Line:
     def __init__(self, point1, point2):
         '''
@@ -23,31 +24,52 @@ class Line:
         self.x2 = point2.x
         self.y2 = point2.y
 
+        self.vertical = False
+        self.horizontal = False
+
         try:
-            self.m = int((self.y2 - self.y1) / (self.x2 - self.x1))
+            self.m = (self.y2 - self.y1) / (self.x2 - self.x1)
             self.b = self.y1 - self.m * self.x1
+
+            if self.m == 0:
+                self.horizontal = True
+
         except ZeroDivisionError:
-            self.m = None
+            self.vertical = True
             self.b = self.x1
     
 
 def intersection(line1, line2):
-    try:
-        try:
-            x = (line2.b - line1.b) / (line1.m - line2.m)
-        except ZeroDivisionError:
-            return None
-        
-        y = line1.m * x + line1.b
-    except TypeError:
-        if line1.m == None:
-            x = line1.b
-            y = line2.b
-        else:
-            x = line2.b
+    x, y = None, None
+
+    if (line1.horizontal and line2.horizontal) or (line1.vertical and line2.vertical):
+        return None
+    
+    else:
+        if line1.horizontal:
             y = line1.b
+        elif line1.vertical:
+            x = line1.b
+        
+        if line2.horizontal:
+            y = line2.b
+        elif line2.vertical:
+            x = line2.b
+
+        if x == None:
+            x = (line2.b - line1.b) / (line1.m - line2.m)
+
+        if y == None:
+            try:
+                y = line1.m * x + line1.b
+            except AttributeError:
+                y = line2.m * x + line2.b     
 
     return (x, y)
+
+def in_map(map_sz, point):
+    x, y = point
+    return (0 <= x < map_sz) and (0 <= y < map_sz)
 
 if __name__ == "__main__":
     # heatmap = fr.Friss(world_sz=(30,30))
@@ -65,10 +87,20 @@ if __name__ == "__main__":
     corner_left_down = Point(sz - 1, 0)
     corner_right_up = Point(0, sz - 1)
     corner_right_down = Point(sz - 1, sz - 1)
-
+    
     edge_top = Line(corner_left_up, corner_right_up)
     edge_left = Line(corner_left_up, corner_left_down)
     edge_bot = Line(corner_left_down, corner_right_down)
     edge_right = Line(corner_right_up, corner_right_down)
 
-    print(intersection(edge_top, edge_bot))
+    map_edges = (edge_top, edge_left, edge_bot, edge_right)    
+
+    origin = Point(1,2)
+    obstacle_vertex = Point(3,1)
+    obs_line = Line(origin, obstacle_vertex)
+
+    for edge in map_edges:
+        intersect_p = intersection(obs_line, edge)
+
+        if intersect_p != None and in_map(sz, intersect_p):
+            print(intersection(obs_line, edge))
