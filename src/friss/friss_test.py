4f7aeed723d53b4675bc930d7688516b4a4a1361
csanrod <c.sanchezro.2018@alumnos.urjc.es>
2023-08-22 11:57:25 +0200

    vertices extraction added, need to fix parallel case

--- src/friss/friss_test.py
+++ src/friss/friss_test.py
@@ -5,6 +5,8 @@ This script is to test the friss module.
 
 import friss as fr
 
+SZ = 5
+
 class Point:
     def __init__(self, x, y):
         '''
@@ -12,6 +14,9 @@ class Point:
         '''
         self.x = x
         self.y = y
+
+    def get_tuple(self):
+        return (self.x, self.y)
 
 
 class Line:
@@ -54,12 +59,26 @@ class Line:
 
         except ZeroDivisionError:
             self.vertical = True
+            self.m = None
             self.b = self.x1
 
             if self.y2 - self.y1 > 0:
                 self.direction = 'N'
             else:
                 self.direction = 'S'
+
+        # self.show_parameters()
+
+
+    def show_parameters(self):
+        print((self.x1, self.y1))
+        print((self.x2, self.y2))
+        print(self.m)
+        print(self.b)
+        print(self.direction)
+        print("horizontal:", self.horizontal)
+        print("vertical:",self.vertical)
+        print()
     
 
 def intersection(line1, line2):
@@ -95,10 +114,96 @@ def in_map(map_sz, point):
     return (0 <= x < map_sz) and (0 <= y < map_sz)
 
 
-def get_polygon_vertices(self, signal_origin, obstacle_vertices, edges):
+def in_direction(reference_p, intersection_p, direction):
+    ref_x = reference_p.x
+    ref_y = reference_p.y
+
+    int_x, int_y = intersection_p
+
+    if direction == 'N':
+        return ref_y <= int_y
+    elif direction == 'S':
+        return ref_y >= int_y
+    elif direction == 'E':
+        return ref_x <= int_x
+    elif direction == 'W':
+        return ref_x >= int_x
+
+    elif direction == 'NE':
+        return ref_x <= int_x and ref_y <= int_y
+    elif direction == 'NW':
+        return ref_x >= int_x and ref_y <= int_y
+    elif direction == 'SE':
+        return ref_x <= int_x and ref_y >= int_y
+    elif direction == 'SW':
+        return ref_x >= int_x and ref_y >= int_y
+
+
+def get_polygon_vertices(signal_origin, obstacle_vertices, edges):
+    vertices = []
+    vertices.append(obstacle_vertices[-1].get_tuple())
+    vertices.append(obstacle_vertices[0].get_tuple())
 
     for obstacle_vertex in obstacle_vertices:
-        Line(signal_origin, obstacle_vertex)
+        l = Line(signal_origin, obstacle_vertex)
+
+        for edge in edges:
+            intersect_p = intersection(l, edge)
+
+            if intersect_p != None and in_map(sz, intersect_p) and in_direction(origin, intersect_p, l.direction):
+                vertices.append(intersect_p)
+
+    if different_edges(vertices[-1], vertices[-2]):
+        # Parallel edges 2 corners??
+        corner = get_corner(vertices[-1], vertices[-2])
+        if corner != None:
+            vertices.insert(-1, corner)
+
+    return vertices
+
+def different_edges(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+
+    return x1 != x2 and y1 != y2
+
+
+def get_corner(p1, p2, sz=SZ):
+    x1, y1 = p1
+    x2, y2 = p2
+
+    if x1 == 0:
+        if x2 == sz - 1:
+            pass
+        elif y2 == 0:
+            return (0, 0)
+        elif y2 == sz - 1:
+            return (0, sz - 1)
+
+    elif x1 == sz - 1:
+        if x2 == 0:
+            pass
+        elif y2 == 0:
+            return (sz - 1, 0)
+        elif y2 == sz - 1:
+            return (sz - 1, sz - 1)
+
+    elif y1 == 0:
+        if x2 == 0:
+            return (0, 0)
+        elif x2 == sz - 1:
+            return (sz - 1, 0)
+        elif y2 == sz - 1:
+            pass
+
+    elif y1 == sz - 1:
+        if x2 == 0:
+            return (0, sz - 1)
+        elif x2 == sz - 1:
+            return (sz - 1, sz - 1)
+        elif y2 == 0:
+            pass
+
 
 
 if __name__ == "__main__":
@@ -126,11 +231,15 @@ if __name__ == "__main__":
     map_edges = (edge_top, edge_left, edge_bot, edge_right)    
 
     origin = Point(1,2)
-    obstacle_vertex = Point(3,1)
-    obs_line = Line(origin, obstacle_vertex)
+    obstacle_vertices = (Point(3,1), Point(0,3))
+    print(get_polygon_vertices(origin, obstacle_vertices, map_edges))
 
-    for edge in map_edges:
-        intersect_p = intersection(obs_line, edge)
 
-        if intersect_p != None and in_map(sz, intersect_p):
-            print(intersection(obs_line, edge))
+    # obstacle_vertex = Point(3,1)
+    # obs_line = Line(origin, obstacle_vertex)
+
+    # for edge in map_edges:
+    #     intersect_p = intersection(obs_line, edge)
+
+    #     if intersect_p != None and in_map(sz, intersect_p) and in_direction(origin, intersect_p, obs_line.direction):
+    #         print(intersect_p)
